4fc6adbc4f6ff0611289e457e664b9d34b130c66
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-02-05 16:10:02 +0100

    feat: add prev and next functionality

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ const songArtist = document.querySelector('.song-artist');
 const songTitle = document.querySelector('.song-title');
 
 const btnPlay = document.querySelector('.btn-play-pause');
+const btnNext = document.querySelector('.btn-next');
+const btnPrev = document.querySelector('.btn-prev');
 
 let isPlay = false;
+let playNum = 0;
 
 const playAudio = () => {
     if (!isPlay) {
@@ -26,5 +29,44 @@ const playAudio = () => {
     }
 }
 
+const playNext = () => {
+    playNum++;
+    console.log("NEXT");
+    if(playNum >= arraySongs.length) {
+        playNum = 0;
+    }
+    song.src = arraySongs[playNum].path;
+    songImg.src = arraySongs[playNum].pathImage;
+    document.body.style.backgroundImage = `url(${arraySongs[playNum].pathImage})`;
+    songArtist.innerHTML = arraySongs[playNum].artist;
+    songTitle.innerHTML = arraySongs[playNum].title;
+
+    isPlay = false;
+
+    playAudio();
+}
+
+const playPrev = () => {
+    playNum--;
+    console.log("NEXT");
+    if(playNum < 0) {
+        playNum = arraySongs.length - 1;
+    }
+
+    song.src = arraySongs[playNum].path;
+    songImg.src = arraySongs[playNum].pathImage;
+    songArtist.innerHTML = arraySongs[playNum].artist;
+    songTitle.innerHTML = arraySongs[playNum].title;
+
+    isPlay = false;
+
+    playAudio();
+}
+
 btnPlay.addEventListener('click', playAudio);
+
+song.addEventListener('ended', playNext);
+
+btnNext.addEventListener('click', playNext);
+btnPrev.addEventListener('click', playPrev);
 
